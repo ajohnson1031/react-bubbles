68daad67ae1695a8be802c36e99f06abc049dd08
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-13 16:17:17 -0400

    created login form and functionality to retrieve token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [loginCreds, setLoginCreds] = useState({
+    username: "",
+    password: "",
+    err: null
+  });
+
+  const handleChange = e => {
+    setLoginCreds({
+      ...loginCreds,
+      [e.target.name]: e.target.value,
+      err: null
+    });
+
+    console.log(
+      `username: ${loginCreds.username}, password: ${loginCreds.password}`
+    );
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    loginCreds.username === "" || loginCreds.password === ""
+      ? setLoginCreds({
+          ...loginCreds,
+          err: "Please complete all login fields."
+        })
+      : axios
+          .post(`http://localhost:5000/api/login`, {
+            username: loginCreds.username,
+            password: loginCreds.password
+          })
+          .then(res => {
+            localStorage.setItem("token", res.data.payload);
+            props.history.push("/bubs");
+          })
+          .catch(err =>
+            setLoginCreds({
+              ...loginCreds,
+              err: "Error logging in. Please try again."
+            })
+          );
+  };
+
   return (
-    <>
+    <div className="login-page">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter username..."
+          value={loginCreds.username}
+          onChange={handleChange}
+          autoComplete="username"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password..."
+          value={loginCreds.password}
+          onChange={handleChange}
+          autoComplete="current-password"
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {loginCreds.err && (
+          <div className="error-container">{loginCreds.err}</div>
+        )}
+      </form>
+    </div>
   );
 };
 
